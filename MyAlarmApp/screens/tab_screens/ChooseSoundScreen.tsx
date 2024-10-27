25ed13a6665d26283c39ff74082fbb12d1b9b19c
Davis <zhongdavis2001@gmail.com>
2024-10-27 01:15:06 -0700

    sound screen update

--- MyAlarmApp/screens/tab_screens/ChooseSoundScreen.tsx
+++ MyAlarmApp/screens/tab_screens/ChooseSoundScreen.tsx
@@ -1,10 +1,18 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React from 'react';
 
 const ChooseSoundScreen = () => {
+  const handlePress = () => {
+    // Handle press event here (e.g., navigate to another screen, play sound, etc.)
+    console.log('Alarm 1 selected');
+  };
+
   return (
     <View style={styles.container}>
       <Text>This is my Sound Options Screen</Text>
+      <TouchableOpacity style={styles.button} onPress={handlePress}>
+        <Text style={styles.buttonText}>Alarm 1</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -14,6 +22,16 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#59F8BB', // Button color
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff', // Text color
+    fontSize: 18,
   },
 });
 
