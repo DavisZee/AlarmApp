43707e8fe9f59051d8440ac9daca1124518c89fb
Davis <zhongdavis2001@gmail.com>
2024-10-27 23:16:55 -0700

    updated

--- MyAlarmApp/screens/tab_screens/ChooseSoundScreen.tsx
+++ MyAlarmApp/screens/tab_screens/ChooseSoundScreen.tsx
@@ -1,21 +1,46 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React from 'react';
 
+
 const ChooseSoundScreen = () => {
-  const handlePress = () => {
-    // Handle press event here (e.g., navigate to another screen, play sound, etc.)
-    console.log('Alarm 1 selected');
+  const saveSoundPath = async (path: string) => {
+    try {
+      await AsyncStorage.setItem("userRingtonePreference", path);
+      console.log(`Sound path saved as user preference: ${path}`);
+    } catch (error) {
+      console.error('Error saving sound path to AsyncStorage', error);
+    }
   };
 
   return (
     <View style={styles.container}>
       <Text>This is my Sound Options Screen</Text>
-      <TouchableOpacity style={styles.button} onPress={handlePress}>
-        <Text style={styles.buttonText}>Alarm 1</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => saveSoundPath('../assets/sound.mp3')}
+      >
+        <Text style={styles.buttonText}>Alarm Sound 1</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => saveSoundPath('../assets/sound.mp3')}
+      >
+        <Text style={styles.buttonText}>Alarm Sound 2</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => saveSoundPath('../assets/sound.mp3')}
+      >
+        <Text style={styles.buttonText}>Alarm Sound 3</Text>
       </TouchableOpacity>
     </View>
   );
 };
+
 
 const styles = StyleSheet.create({
   container: {
@@ -27,10 +52,10 @@ const styles = StyleSheet.create({
     marginTop: 20,
     padding: 10,
     backgroundColor: '#59F8BB', // Button color
-    borderRadius: 5,
+    borderRadius: 5
   },
   buttonText: {
-    color: '#fff', // Text color
+    color: '#000000', // Text color
     fontSize: 18,
   },
 });
